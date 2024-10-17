1ad1fbf2e06b62db1c663c2efab46705750b9b81
jeffnaco <150464851+jeffnaco@users.noreply.github.com>
2024-10-17 13:17:53 +0100

    Add files via upload

---
+++ SortingAlgorithms/SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class BubbleSort
+    {
+        public static void Perform(List<double> listToSort)
+        {
+            bool swapped = true;
+            double temp;
+
+            while (swapped)
+            {
+                swapped = false;
+                for (int i = 0; i < listToSort.Count - 1; i++)
+                {
+                    if (listToSort[i] > listToSort[i + 1])
+                    {
+                        temp = listToSort[i];
+                        listToSort[i] = listToSort[i + 1];
+                        listToSort[i+1] = temp;
+                        swapped = true;
+                    }  
+                }
+            }
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class Program
+    {
+        static Random randomGenerator = new Random();
+        static List<double> listOfDoubles = new List<double>();
+
+        static void Main(string[] args)
+        {
+            TenDoubles();
+            MenuSystem mainmenu = new MenuSystem("Program");
+
+            mainmenu.AddMenuItem("Randomise List of Doubles (10 Values)", "TenDoubles", typeof(Program));
+            mainmenu.AddMenuItem("Print List of Doubles", "PrintListOfDoubles", typeof(Program));
+            mainmenu.AddMenuItem("Perform a Bubble Sort", "PerformBubble", typeof(Program));
+
+            mainmenu.RunForever();
+            Console.ReadKey();
+        }
+
+        public static void PerformBubble()
+        {
+            Console.WriteLine("Performing Bubble sort...");
+            BubbleSort.Perform(listOfDoubles);
+            Console.WriteLine("Sort completed");
+        }
+
+        public static void TenDoubles()
+        {
+            Console.WriteLine("Populating list with random numbers...");
+            PopulateListWithRandomDoubles(10);
+            Console.WriteLine("List populated");
+
+        }
+
+        static void PopulateListWithRandomDoubles(int size)
+        {
+            listOfDoubles.Clear();
+            for (int i = 0; i < size; i++)
+            {
+                double twoDigitDouble = Double.Parse(randomGenerator.NextDouble().ToString("0.00000"));
+                listOfDoubles.Add(twoDigitDouble);
+            }
+        }
+
+        public static void PrintListOfDoubles()
+        {
+            Console.WriteLine("\n\nLIST PRINT:\n");
+
+            for (int i = 0; i < listOfDoubles.Count; i++)
+            {
+                Console.WriteLine(" " + i + " " + listOfDoubles[i].ToString());
+
+            }
+            Console.WriteLine("\nEND\n");
+        }
+    }
+}
